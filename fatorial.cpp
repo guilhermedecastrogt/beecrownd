5711c6af154b91f26c08d6e74cc996d892ee9f21
Guilherme de Castro <guilhermecastro1000@gmail.com>
2023-12-18 01:03:34 -0300

    UpdateFatorial

--- fatorial.cpp
+++ fatorial.cpp
@@ -1,46 +1,34 @@
+#include <stdio.h>
+
+int fatorial (int n) {
+    return (n > 0) ? n*fatorial(n-1) : 1;
+}
 /*
- *      Um investidor deseja aplicar um valor inicial de R$ 10.000,00 em um
- * fundo de investimento que rende 10% ao ano. Ele deseja saber quanto terá
- * acumulado no final de n anos.
- *
- *
- *      Para resolver este problema, podemos usar uma função recursiva para
- * calcular o fatorial de n. A função recursiva deve funcionar da seguinte forma:
- *
- *      Se n for igual a 0, então o fatorial de n é igual a 1.
- *      Caso contrário, o fatorial de n é igual a n multiplicado pelo fatorial de n-1.
- * */
+* Em uma escola, dos x alunos classificados para a OBMEP,
+* n serão premiados por um sorteio realizado pela instituição.
+* Os sorteados receberão uma cesta de café da manhã.
+* De quantas maneiras distintas a dupla premiada pode ocorrer?
+*
+* Estamos calculando a combinação de x elementos tomados de n em n.
+*/
 
+int main() {
+    int alunos, sorteados;
+    int C;
+    printf("Digite o numero de alunos que classificaram para a OBMEP: ");
+    scanf("%d", &alunos);
+    printf("\nDigite a quantidade de alunos a serem sorteados: ");
+    scanf("%d", &sorteados);
+    printf("\nAgora, vamos calcular de quantas maneiras destintas podemos sortear %d destes alunos.\n", sorteados);
 
-#include <iostream>
-#include <cmath>
-
-using namespace std;
-
-// Função recursiva para calcular o fatorial de n
-int calcularFatorial(int n) {
-    if (n == 0) {
+    if (sorteados > alunos) {
+        printf("O numero de sorteados nao pode ser maior que o numero de alunos! Reinicie o programa...");
         return 1;
     }
 
-    // Chamada recursiva
-    return n * calcularFatorial(n - 1);
-}
+    C = fatorial(alunos)/(fatorial(sorteados)*fatorial(alunos-sorteados));
 
-int main() {
-
-    int n;
-    double valor_acumulado;
-
-    cout << "Digite o número de anos: ";
-    cin >> n;
-
-    // Calculando o fatorial de n
-    int fatorial = calcularFatorial(n);
-
-    valor_acumulado = 10000 * pow(1.1, n);
-
-    cout << "O valor acumulado será de R$ " << valor_acumulado << endl;
+    printf("Considerando uma combinacao de %d tomados por %d a %d, temos que: %d\n", alunos, sorteados, sorteados, C);
 
     return 0;
 }
