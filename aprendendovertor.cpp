0c94dd1c0f8d175be01b176fb5259fab8f8e6f66
Guilherme de Castro <guilhermecastro1000@gmail.com>
2024-02-10 23:54:26 -0300

    aprendendovetor.cpp

---
+++ aprendendovertor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int meuvetor[] = {1, 20, 3, 7};
+    int meuvetor2 = meuvetor[3];
+
+    for(int i = 0; i < meuvetor2; i++){
+
+        cout<< meuvetor2<<" ";
+
+    }
+}
