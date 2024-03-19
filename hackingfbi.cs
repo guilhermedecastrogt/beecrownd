d5846d998d2bcf3079efb185b7f772d4371ecd3e
Guilherme de Castro <guilhermecastro1000@gmail.com>
2024-03-19 16:12:56 -0300

    hackingfbimoovie.cs

---
+++ hackingfbi.cs
@@ -0,0 +1,14 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.Black;
+
+int c = 0;
+for (int i = 1; i <= 200; i = Random.Shared.Next(i+1, i + 10))
+{
+    if (i > 100) i = 100;
+    Console.WriteLine($"Hacking FBI Database... {i}%");
+    Thread.Sleep(Random.Shared.Next(50, 200));
+    c = i;
+    if (i == 100) break;
+}
+
+Console.WriteLine("Hacking FBI Database Successful!");
